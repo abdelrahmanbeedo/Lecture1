970fea57c94987fdc4d1dd2d06137c4dc2b0c878
beedo <abdelrahman.mahmoud23b@eslsca.edu.eg>
2025-09-24 11:52:52 +0300

    test2

--- Assets/Project/testscript2.cs
+++ Assets/Project/testscript2.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class testscript : MonoBehaviour
+public class testscript2 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
